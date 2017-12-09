f28daf0fceb547e3d2878e1096da0e47fe88370c
Patrick <pat.rodriguez108@gmail.com>
2017-12-09 14:06:48 -0600

    Solve algorithm

--- valid_parens.rb
+++ valid_parens.rb
@@ -1,3 +1,32 @@
+# check if string has complete set of parens
+# store parens in array
+# join array
+# get length of array
+
 def longest_valid_parentheses(s)
-    
+	set = "()"
+	parens = []
+	if s.include?(set)
+		i = 0
+		while s.length > i
+			if s[i] == set[0]
+				if s[i + 1] == set[1]
+					parens << "#{s[i]}#{s[i + 1]}"
+				end
+			end
+			i += 1
+		end
+	end
+	parens.join.split('').count
 end
+
+# exam_one = "()"
+
+# longest_valid_parentheses(exam_one)
+
+exam_two = "(()"
+
+p longest_valid_parentheses(exam_two)
+
+exam_three = ")()())"
+
